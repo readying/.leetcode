d971f06d79ee8550f6d11253e7d3ba9b81183f67
readying <chenxiaomp3@163.com>
2021-06-22 08:34:09 +0800

    3 temp

--- 3.无重复字符的最长子串.java
+++ 3.无重复字符的最长子串.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 /*
  * @lc app=leetcode.cn id=3 lang=java
  *
@@ -7,7 +9,6 @@
 // @lc code=start
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-
 
         if(s == null || s.length() == 0){
             return 0;
@@ -20,27 +21,39 @@ class Solution {
         int l = 0, r = 1;
         int resultNum = 0;
 
-        HashMap<Character, Integer> hashMap = new HashMap<>();
-        HashSet<Character> sets = new HashSet<>();
-        hashMap.put(s.charAt(l), 0);
-        sets.add(s.charAt(l));
+        // HashMap<Character, Integer> hashMap = new HashMap<>();
+        // HashSet<Character> sets = new HashSet<>();
+        // hashMap.put(s.charAt(l), 0);
+        // sets.add(s.charAt(l));
+
+        StringBuffer sb = new StringBuffer();
+        sb.append(s.charAt(l));
         
-        for(l = 0, r = 1; l < length - 1; r++){
-            if(r >= length) {
-                break;
-            }
+        for(l = 0, r = 1; r < length ; r++){
+            // if(r >= length) {
+            //     break;
+            // }
            Character ch = s.charAt(r);
-           if (sets.contains(ch)) {
-               int temp = r - l ;
-               resultNum = Math.max(temp, resultNum);
-               l = hashMap.get(ch) + 1 ;
-               hashMap.put(ch, r);
+           int temp;
+           if (s.substring(l, r).contains(ch)) {
+            //    temp = r - l ;
+               int k = s.indexOf(ch);
+               temp = s.length();
+               s = s.substring(k, temp -1);
+               s.append(ch);
+               l = indexOf
+            //    l = hashMap.get(ch) + 1 ;
             //    sets.remove(ch);
            }else {
-               hashMap.put(ch, r);
                sets.add(ch);
+               temp = r - l + 1;
            }
+            resultNum = Math.max(temp, resultNum);
+            hashMap.put(ch, r);
         }
+        // if(resultNum == 0){
+        //     return sets.size();
+        // }
 
         return resultNum; 
 
