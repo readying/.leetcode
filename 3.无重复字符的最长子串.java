64b3d08e00b378f511291e8ffd911d87e2438765
readying <chenxiaomp3@163.com>
2021-06-24 07:05:42 +0800

    3 ok

--- 3.无重复字符的最长子串.java
+++ 3.无重复字符的最长子串.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.HashSet;
 
 /*
@@ -8,6 +9,7 @@ import java.util.HashSet;
 
 // @lc code=start
 class Solution {
+
     public int lengthOfLongestSubstring(String s) {
 
         if(s == null || s.length() == 0){
@@ -18,46 +20,65 @@ class Solution {
             return 1;
         }
 
-        int l = 0, r = 1;
-        int resultNum = 0;
+        int l = 0, res = 0;
 
-        // HashMap<Character, Integer> hashMap = new HashMap<>();
-        // HashSet<Character> sets = new HashSet<>();
-        // hashMap.put(s.charAt(l), 0);
-        // sets.add(s.charAt(l));
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        hashMap.put(s.charAt(l), 0);
 
-        StringBuffer sb = new StringBuffer();
-        sb.append(s.charAt(l));
-        
-        for(l = 0, r = 1; r < length ; r++){
-            // if(r >= length) {
-            //     break;
-            // }
-           Character ch = s.charAt(r);
-           int temp;
-           if (s.substring(l, r).contains(ch)) {
-            //    temp = r - l ;
-               int k = s.indexOf(ch);
-               temp = s.length();
-               s = s.substring(k, temp -1);
-               s.append(ch);
-               l = indexOf
-            //    l = hashMap.get(ch) + 1 ;
-            //    sets.remove(ch);
-           }else {
-               sets.add(ch);
-               temp = r - l + 1;
-           }
-            resultNum = Math.max(temp, resultNum);
+        for(int r = 1; r < length; r++){
+            char ch = s.charAt(r);
+            if(hashMap.containsKey(ch)){
+                // 这个是核心, 我们不去删除left元素之前的元素，但是如果再次找到的元素是在他的左边，我们就忽略即可。
+                l = Math.max(l, hashMap.get(ch)+1);
+            }
             hashMap.put(ch, r);
+            res = Math.max(res, r-l+1);
         }
-        // if(resultNum == 0){
-        //     return sets.size();
-        // }
 
-        return resultNum; 
-
+        return res;
     }
+
+
+    /**
+     * 第一种
+     *  主要是利用了滑动窗口指针 左指针的移动
+     * @param s
+     * @return
+     */
+    // public int lengthOfLongestSubstring(String s) {
+
+    //     if(s == null || s.length() == 0){
+    //         return 0;
+    //     }
+    //     int length = s.length();
+    //     if(length == 1){
+    //         return 1;
+    //     }
+
+    //     int l = 0, r = 1;
+    //     int resultNum = 0;
+
+    //     // HashMap<Character, Integer> hashMap = new HashMap<>();
+    //     // HashSet<Character> sets = new HashSet<>();
+    //     // hashMap.put(s.charAt(l), 0);
+    //     // sets.add(s.charAt(l));
+
+    //     for(l = 0, r = 1; r < length ; r++){
+    //        Character ch = s.charAt(r);
+    //        int temp;
+    //        int pos;
+    //        if ((pos = s.substring(l, r).indexOf(ch)) > -1) {
+    //            temp = r - l ;
+    //            l = pos + l + 1;
+    //         //    l = hashMap.get(ch) + 1 ;
+    //         //    sets.remove(ch);
+    //        }else {
+    //            temp = r - l + 1;
+    //        }
+    //         resultNum = Math.max(temp, resultNum);
+    //     }
+    //     return resultNum; 
+    // }
 }
 // @lc code=end
 
